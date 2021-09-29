528e4ac4cb5a3d1a74119348d4d4989f744609c1
ynitami <80726867+ynitami@users.noreply.github.com>
2021-09-29 10:35:16 +0900

    草プロ0929

--- main.js
+++ main.js
@@ -1,51 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  query, 
-  orderBy,
-  limit
-} from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC96vZD-UvNInuaJYKik47PcTRhjjEEpJY",
-  authDomain: "bike-rider-fd6c2.firebaseapp.com",
-  projectId: "bike-rider-fd6c2",
-  storageBucket: "bike-rider-fd6c2.appspot.com",
-  messagingSenderId: "205491184817",
-  appId: "1:205491184817:web:35c5db0067993fa5de2e90",
-  measurementId: "G-8WZF58EYDW"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-const db = getFirestore(app);
-const scoresRef = collection(db, "scores");
-const scores = document.getElementById("scores");
-// データを1つのオブジェクトにまとめる
-// query(collectionRef, rule1, rule2, ...)
-getDocs(query(scoresRef, orderBy("score", "desc"), limit(3))).then((snap) => {
-  // arrow関数でobjectをすぐに返す場合は()をつける
-  // doc.id === "kxaKJf88Ql8zuIkjJhr9"
-  // doc.data() === {score: 250}
-  const data = snap.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-  data.forEach(number => {
-    const newDiv = document.createElement("div");
-    newDiv.appendChild(document.createTextNode(number.score));
-    scores.appendChild(newDiv);
-  })
-  console.log(data)
-})
-
 /**
  * @type {HTMLCanvasElement}
 */
@@ -221,15 +173,11 @@ function main() {
           jumpCount = 0;
         } else { // 着地失敗
           clearInterval(game);
-          // ドキュメントを追加
-          addDoc(scoresRef, { score: courseStartIndex + 1 });
         }
       }
     } else { // prevPlayerY === prevCourseHeight
       if (prevPlayerY === 0) { //落下済み
         clearInterval(game);
-        // ドキュメントを追加
-        addDoc(scoresRef, { score: courseStartIndex + 1 });
       } else if (upPressed) { // コース上からの鉛直投げ上げ運動
         timeAfterJump = 0;
         nextPlayerY = prevPlayerY + dy(timeAfterJump);
